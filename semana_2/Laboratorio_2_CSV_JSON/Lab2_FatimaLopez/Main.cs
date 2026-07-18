dadbec724e56a33885b58f2bbbaa66040c1ad10b
Fatima Lopez <fjlopezqui@correo@url.edu.gt>
2026-07-17 19:15:27 -0600

    feat: Lab 2: Convertir csv a json desde consola c#

---
+++ semana_2/Laboratorio_2_CSV_JSON/Lab2_FatimaLopez/Main.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+
+class Lab2
+{
+   static void Main()
+   {
+       List<Estudiante> listaEstudiantes = new List<Estudiante>();
+       string[] lineasCSVest = File.ReadAllLines("estudiantes.csv");
+       for (int i = 1; i < lineasCSVest.Length; i++)
+        {
+            string[] datosEstudiante = lineasCSVest[i].Split(',');
+
+            Estudiante nuevoEstudiante = new Estudiante 
+            { 
+                    Id = Convert.ToInt32(datosEstudiante[0]),
+                    Nombre = datosEstudiante[1],
+                    Carrera = datosEstudiante[2]
+            };
+
+            listaEstudiantes.Add(nuevoEstudiante);
+
+        }
+
+        foreach (Estudiante est in listaEstudiantes)
+        {
+            Console.WriteLine($"ID: {est.Id} | Nombre: {est.Nombre} | Carrera: {est.Carrera}");
+            Console.WriteLine("-----------------------------------------------------------------------------------");
+        }
+
+        string textoJSON = JsonSerializer.Serialize(listaEstudiantes, new JsonSerializerOptions {WriteIndented = true});
+
+        File.WriteAllText("estudiantes.json", textoJSON);
+
+   }
+ 
+}
